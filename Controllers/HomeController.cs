83eae92766e533a184e7f2e12d20e26d35db9b75
Hendrii_ <hendri6susanto.hs@gmail.com>
2020-12-17 15:41:26 +0700

    add file

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.ViewModels;
+
+namespace ContosoUniversity.Controllers
+{
+
+
+    public class HomeController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to Contoso University";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+            var data = from student in db.Students
+                     group student by student.EnrollmentDate into dateGroup
+                   select new EnrollmentDateGroup()
+                 {
+                   EnrollmentDate = dateGroup.Key,
+                 StudentCount = dateGroup.Count()
+            };
+            return View(data);
+       //     var query = "SELECT EnrollmentDate, COUNT(*) AS StudentCount "
+   // + "FROM Person "
+    //+ "WHERE EnrollmentDate IS NOT NULL "
+    //+ "GROUP BY EnrollmentDate";
+      //      var data = db.Database.SqlQuery<EnrollmentDateGroup>(query);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DAL/IStudentRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.DAL
+{
+    interface IStudentRepository : IDisposable
+    {
+
+        
+
+        IEnumerable<Student> GetStudents();
+        Student GetStudentByID(int studentId);
+        void InsertStudent(Student student);
+        void DeleteStudent(int studentID);
+        void UpdateStudent(Student student);
+        void Save();
+    }
+}
